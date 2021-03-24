2cd516f3a5351ba942b1a4370b2cf04496533f8a
KaterynaKub <ekaterina.kubyshka@gmail.com>
2021-03-24 18:40:34 +0200

    Add Ajax-request

--- frontend/public/js/controllers/PizzaController.js
+++ frontend/public/js/controllers/PizzaController.js
@@ -1,4 +1,6 @@
 app.controller('PizzaController', 
-    function($scope){
-
-})
+    function($scope, $http){
+        $http({method:"GET", url: "http://localhost:8080"}).then(function success(response){
+            $scope.message = response.data.message;
+        });
+});
