73ebf33e01e447ca86697fa50c80a231a0e703e9
Dekardt <sasha32083@gmail.com>
2021-05-21 16:27:57 +0300

    Add pizza-model

--- backend/serializers.py
+++ backend/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
-from .models import Drink
+from .models import *
 
 
 class DrinkSerializer(serializers.ModelSerializer):
     class Meta:
         model = Drink
         fields = ('itemClass', 'items')
+
+class PitsaClassSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = PitsaClass
+        fields = ('itemClass',
+                'items')
--- backend/submodels/__init__.py
+++ backend/submodels/__init__.py
@@ -1,1 +1,2 @@
 from .drink_model import *
+from .pizza_model import *
